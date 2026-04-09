180fdf850204c1316dc25fe93f2ad048e71407f8
Prithivi <prithivi@users.noreply.github.com>
2026-04-09 13:39:11 +0545

    feat: update package.json and add Gemini configuration with default model

--- backend/src/config/gemini.config.js
+++ backend/src/config/gemini.config.js
@@ -1,5 +1,20 @@
 import { GoogleGenAI } from "@google/genai";
+import { DEFAULT_MODEL } from "../utils";
 
-export const ai = new GoogleGenAI({
+const ai = new GoogleGenAI({
   apiKey: process.env.GOOGLE_API_KEY,
 });
+
+export async function generateResponse(prompt, model = DEFAULT_MODEL) {
+  try {
+    const respone = await ai.models.generateContent({
+      model: model,
+      contents: prompt,
+    });
+
+    return respone.text;
+  } catch (error) {
+    throw error;
+    console.error("Error generating response:", error);
+  }
+}
---
+++ backend/src/utils/index.js
@@ -0,0 +1,9 @@
+export const MODEL = Object.freeze({
+  GEMIPRO: "gemini-2.5-pro",
+  GEMI_FLASH: "gemini-2.5-flash",
+  GEMI_FLASH_PREVIEW_09_2025: "gemini-2.5-flash-preview-09-2025",
+  GEMI_FLASH_LITE: "gemini-2.5-flash-lite",
+  GEMI_FLASH_LITE_PREVIEW_09_2025: "gemini-2.5-flash-lite-preview-09-2025",
+});
+
+export const DEFAULT_MODEL = MODEL.GEMI_FLASH_LITE;
